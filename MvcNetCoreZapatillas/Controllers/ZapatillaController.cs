c113021df788c6528a92b93071aa93b5ad8036e7
GiovannyCortes <giovannyandres.cortes@outlook.com>
2023-03-16 14:27:09 +0100

    Add project files.

---
+++ MvcNetCoreZapatillas/Controllers/ZapatillaController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcNetCoreZapatillas.Models;
+using MvcNetCoreZapatillas.Repositories;
+
+namespace MvcNetCoreZapatillas.Controllers {
+    public class ZapatillaController : Controller {
+
+        private RepositoryZapatillas repo;
+
+        public ZapatillaController(RepositoryZapatillas repo) {
+            this.repo = repo;
+        }
+
+        public async Task<IActionResult> GetZapatillas() {
+            List<Zapatilla> zapatillas = await this.repo.GetZapatillas();
+            return View(zapatillas);
+        }
+
+        public async Task<IActionResult> ZapatillaDetalle(int idproducto) {
+            Zapatilla zapatilla = await this.repo.FindZapatilla(idproducto);
+            return View(zapatilla);
+        }
+
+        public async Task<IActionResult> _DetailsZapatilla(int idproducto, int? posicion) {
+            if (posicion == null) {
+                posicion = 1;
+            }
+
+            int numregistros = await this.repo.GetMaxRegistros(idproducto);
+
+            int siguiente = posicion.Value + 1;
+                siguiente = (siguiente > numregistros) ? 1 : siguiente;
+            
+            int anterior = posicion.Value - 1;
+                anterior = (anterior < 1) ? numregistros : anterior;
+
+            ImagenZapatilla imagenZapatilla = this.repo.GetImagenZapatilla(idproducto, posicion.Value);
+
+            ViewData["ULTIMO"] = numregistros;
+            ViewData["SIGUIENTE"] = siguiente;
+            ViewData["ANTERIOR"] = anterior;
+            
+            return PartialView("_DetailsZapatilla", imagenZapatilla);
+        }
+
+    }
+}
---
+++ MvcNetCoreZapatillas/Repositories/RepositoryZapatillas.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using MvcNetCoreZapatillas.Data;
+using MvcNetCoreZapatillas.Models;
+
+#region PROCEDURES SQL SERVER
+/*
+ 
+    CREATE PROCEDURE SP_GETIMG_ZAPAS(@IDPRODUCTO INT, @POSICION INT)
+    AS
+	    SELECT * FROM (
+		    SELECT CAST(ISNULL(
+			    ROW_NUMBER() OVER (ORDER BY IDIMAGEN),  0) AS INT)
+			    AS INDICE,
+		    IDIMAGEN, IDPRODUCTO, IMAGEN
+		    FROM IMAGENESZAPASPRACTICA
+		    WHERE IDPRODUCTO = @IDPRODUCTO
+	    ) AS QUERY
+	    WHERE INDICE >= @POSICION AND INDICE < (@POSICION + 1)
+    GO
+
+ */
+#endregion
+
+namespace MvcNetCoreZapatillas.Repositories{
+    public class RepositoryZapatillas{
+        
+        private ZapatillasContext context;
+
+        public RepositoryZapatillas(ZapatillasContext context) {
+            this.context = context;
+        }
+
+        public async Task<List<Zapatilla>> GetZapatillas() {
+            return await this.context.Zapatillas.ToListAsync();
+        } 
+
+        public async Task<Zapatilla> FindZapatilla(int idproducto) {
+            return await this.context.Zapatillas.FirstOrDefaultAsync(x => x.IdProducto == idproducto);
+        }
+
+        public async Task<int> GetMaxRegistros(int idproducto) {
+            var consulta = await this.context.ImagenesZapatillas.Where(x => x.IdProducto == idproducto).ToListAsync();
+            return consulta.Count();
+        }
+
+        public ImagenZapatilla GetImagenZapatilla(int idproducto, int posicion) {
+            string sql = "SP_GETIMG_ZAPAS @IDPRODUCTO, @POSICION";
+            
+            SqlParameter pamidproducto = new SqlParameter("@IDPRODUCTO", idproducto);
+            SqlParameter pamposicion = new SqlParameter("@POSICION", posicion);
+
+            var consulta = this.context.ImagenesZapatillas.FromSqlRaw(sql, pamidproducto, pamposicion);
+            ImagenZapatilla imagenZapatilla = consulta.ToList().FirstOrDefault();
+            return imagenZapatilla;
+        }
+
+    }
+}
